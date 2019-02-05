c9d341172f88cb8edabdc3e4cee75de6e08e0b03
Andrew Olson <anolson@gmail.com>
2019-02-04 21:58:04 -0500

    Rename util/test_help.go -> util/float.go

--- util/float.go
+++ util/float.go
@@ -4,7 +4,7 @@ import "math"
 
 // Approx returns true if two float values are approximately equal
 func Approx(a, b float64) bool {
-	epsilon := 0.00001
+	epsilon := 1e-5
 
 	return math.Abs(a-b) < epsilon
 }
