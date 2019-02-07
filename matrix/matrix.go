bceec29d56e4ae88f7fc92b7b5c6c24b248eb5da
Andrew Olson <anolson@gmail.com>
2019-02-06 21:57:07 -0500

    Add matrix multiplication.

--- matrix/matrix.go
+++ matrix/matrix.go
@@ -1,6 +1,8 @@
 package matrix
 
-import "github.com/anolson/rtc/util"
+import (
+	"github.com/anolson/rtc/util"
+)
 
 type Matrix struct {
 	rows int
@@ -30,6 +32,14 @@ func (m *Matrix) At(i, j int) float64 {
 	return m.data[index]
 }
 
+func (m *Matrix) Set(i, j int, value float64) {
+	index := m.cols*i + j
+
+	if index < len(m.data) {
+		m.data[index] = value
+	}
+}
+
 func (m *Matrix) Equal(other *Matrix) bool {
 	for i := 0; i < m.rows; i++ {
 		for j := 0; j < m.cols; j++ {
@@ -40,4 +50,44 @@ func (m *Matrix) Equal(other *Matrix) bool {
 	}
 
 	return true
+}
+
+func (m *Matrix) Row(index int) []float64 {
+	offset := m.cols * index
+
+	if offset >= len(m.data) {
+		return nil
+	}
+
+	return m.data[offset : offset+m.cols]
+}
+
+func (m *Matrix) Col(index int) []float64 {
+	col := []float64{}
+
+	for i := index; i < len(m.data); i += m.cols {
+		col = append(col, m.data[i])
+	}
+
+	return col
+}
+
+func (m *Matrix) Multiply(other *Matrix) *Matrix {
+	result := New(m.rows, m.cols, nil)
+
+	for i := 0; i < m.rows; i++ {
+		for j := 0; j < m.cols; j++ {
+			row := m.Row(i)
+			col := other.Col(j)
+
+			var accumulator float64
+			for k := 0; k < m.rows; k++ {
+				accumulator = accumulator + (row[k] * col[k])
+			}
+
+			result.Set(i, j, accumulator)
+		}
+	}
+
+	return result
 }
--- matrix/matrix_test.go
+++ matrix/matrix_test.go
@@ -74,3 +74,56 @@ func TestEqual(t *testing.T) {
 	assert.True(t, m.Equal(other))
 	assert.False(t, m.Equal(otherNotEqual))
 }
+
+func TestRow(t *testing.T) {
+	m := New(3, 4, []float64{
+		1.0, 2.0, 3.0, 4.0,
+		5.0, 6.0, 7.0, 8.0,
+		9.0, 8.0, 7.0, 6.0,
+		5.0, 4.0, 3.0, 2.0,
+	})
+
+	assert.Equal(t, []float64{1.0, 2.0, 3.0, 4.0}, m.Row(0))
+	assert.Equal(t, []float64{5.0, 6.0, 7.0, 8.0}, m.Row(1))
+	assert.Equal(t, []float64{9.0, 8.0, 7.0, 6.0}, m.Row(2))
+	assert.Equal(t, []float64{5.0, 4.0, 3.0, 2.0}, m.Row(3))
+}
+
+func TestCol(t *testing.T) {
+	m := New(4, 4, []float64{
+		1.0, 2.0, 3.0, 4.0,
+		5.0, 6.0, 7.0, 8.0,
+		9.0, 8.0, 7.0, 6.0,
+		5.0, 4.0, 3.0, 2.0,
+	})
+
+	assert.Equal(t, []float64{1.0, 5.0, 9.0, 5.0}, m.Col(0))
+	assert.Equal(t, []float64{2.0, 6.0, 8.0, 4.0}, m.Col(1))
+	assert.Equal(t, []float64{3.0, 7.0, 7.0, 3.0}, m.Col(2))
+	assert.Equal(t, []float64{4.0, 8.0, 6.0, 2.0}, m.Col(3))
+}
+
+func TestMultiply(t *testing.T) {
+	a := New(4, 4, []float64{
+		1.0, 2.0, 3.0, 4.0,
+		5.0, 6.0, 7.0, 8.0,
+		9.0, 8.0, 7.0, 6.0,
+		5.0, 4.0, 3.0, 2.0,
+	})
+
+	b := New(4, 4, []float64{
+		-2.0, 1.0, 2.0, 3.0,
+		3.0, 2.0, 1.0, -1.0,
+		4.0, 3.0, 6.0, 5.0,
+		1.0, 2.0, 7.0, 8.0,
+	})
+
+	result := New(4, 4, []float64{
+		20.0, 22.0, 50.0, 48.0,
+		44.0, 54.0, 114.0, 108.0,
+		40.0, 58.0, 110.0, 102.0,
+		16.0, 26.0, 46.0, 42.0,
+	})
+
+	assert.Equal(t, result, a.Multiply(b))
+}
