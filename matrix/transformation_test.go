7cc35477ea765b047010b6414b141d9684e5be95
Andrew Olson <anolson@gmail.com>
2019-02-18 14:49:43 -0500

    Update tests.

--- matrix/transformation_test.go
+++ matrix/transformation_test.go
@@ -7,15 +7,22 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestTranslationMatrix(t *testing.T) {
-	t.Run("Multiplying by a translation matrix", func(t *testing.T) {
+func TestTranslation(t *testing.T) {
+	t.Run("Applying a translation matrix to a point", func(t *testing.T) {
 		transform := Translation(5, -3, 2)
 		p := tuple.Point(-3, 4, 5)
 
 		assert.Equal(t, tuple.Point(2, 1, 7), MultiplyByTuple(transform, p))
 	})
 
-	t.Run("Multiplying by inverse of a translation matrix", func(t *testing.T) {
+	t.Run("Applying a translation matrix to a vector - has no effect", func(t *testing.T) {
+		transform := Translation(5, -3, 2)
+		v := tuple.Vector(-3, 4, 5)
+
+		assert.Equal(t, v, MultiplyByTuple(transform, v))
+	})
+
+	t.Run("Applying the inverse of a translation matrix", func(t *testing.T) {
 		transform := Translation(5, -3, 2)
 		inverse, err := Inverse(transform)
 		assert.Nil(t, err)
@@ -23,16 +30,9 @@ func TestTranslationMatrix(t *testing.T) {
 
 		assert.Equal(t, tuple.Point(-8, 7, 3), MultiplyByTuple(inverse, p))
 	})
-
-	t.Run("Translation does not affect vector", func(t *testing.T) {
-		transform := Translation(5, -3, 2)
-		v := tuple.Vector(-3, 4, 5)
-
-		assert.Equal(t, v, MultiplyByTuple(transform, v))
-	})
 }
 
-func TestScalingMatrix(t *testing.T) {
+func TestScaling(t *testing.T) {
 	t.Run("Applying a scaling matrix to a point", func(t *testing.T) {
 		transform := Scaling(2, 3, 4)
 		p := tuple.Point(-4, 6, 8)
