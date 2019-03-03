c96d26bff89621a5a32566612bdc81725042b65e
Andrew Olson <anolson@gmail.com>
2019-03-02 22:11:54 -0500

    Add intersection Hit() function.

--- primitives/intersection.go
+++ primitives/intersection.go
@@ -1,5 +1,7 @@
 package primitives
 
+import "sort"
+
 // Intersection aggregates the object and t values
 type Intersection struct {
 	t      float64
@@ -12,4 +14,19 @@ func NewIntersection(t float64, object Object) *Intersection {
 		t:      t,
 		object: object,
 	}
+}
+
+// Hit identifies which intersection is visible from the ray's origin
+func Hit(intersections []*Intersection) *Intersection {
+	sort.Slice(intersections, func(i, j int) bool {
+		return intersections[i].t < intersections[j].t
+	})
+
+	for _, intersection := range intersections {
+		if intersection.t > 0 {
+			return intersection
+		}
+	}
+
+	return nil
 }
--- primitives/intersection_test.go
+++ primitives/intersection_test.go
@@ -24,3 +24,43 @@ func TestIntersectionAggregation(t *testing.T) {
 	assert.Equal(t, float64(1), intersections[0].t)
 	assert.Equal(t, float64(2), intersections[1].t)
 }
+
+func TestHit(t *testing.T) {
+	t.Run("The hit, when all intersections have positive t values", func(t *testing.T) {
+		s := NewSphere()
+		i1 := NewIntersection(1, s)
+		i2 := NewIntersection(2, s)
+
+		intersections := []*Intersection{i2, i1}
+		assert.Equal(t, i1, Hit(intersections))
+	})
+
+	t.Run("The hit, when some intersections have negative t values", func(t *testing.T) {
+		s := NewSphere()
+		i1 := NewIntersection(-1, s)
+		i2 := NewIntersection(1, s)
+
+		intersections := []*Intersection{i2, i1}
+		assert.Equal(t, i2, Hit(intersections))
+	})
+
+	t.Run("The hit, when all intersections have negative t values", func(t *testing.T) {
+		s := NewSphere()
+		i1 := NewIntersection(-2, s)
+		i2 := NewIntersection(-1, s)
+
+		intersections := []*Intersection{i2, i1}
+		assert.Nil(t, Hit(intersections))
+	})
+
+	t.Run("Is always the non-negative t value", func(t *testing.T) {
+		s := NewSphere()
+		i1 := NewIntersection(5, s)
+		i2 := NewIntersection(7, s)
+		i3 := NewIntersection(-3, s)
+		i4 := NewIntersection(2, s)
+
+		intersections := []*Intersection{i1, i2, i3, i4}
+		assert.Equal(t, i4, Hit(intersections))
+	})
+}
